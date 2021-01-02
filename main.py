c2375b2074e1a611fd5665f5c53b81f65098e287
vlod <vladalex420@gmail.com>
2021-01-02 14:11:19 +0200

    calculated rmax diff

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from pprint import pprint
 from bs4 import BeautifulSoup
 from pcData import *
+import pygame
 
 def medieLuna(date):
     r = requests.get('https://www.top500.org/lists/top500/' + date)
@@ -26,7 +27,7 @@ def medieLuna(date):
             medie.cores +=  int(tableData[2].text.replace(',',""))
             medie.rmax +=   float(tableData[3].text.replace(',',""))
             medie.rpeak +=  float(tableData[4].text.replace(',',""))
-            medie.power +=  float(tableData[5].text.replace(',',""))
+            #medie.power +=  float(tableData[5].text.replace(',',""))
             #print(tableData[2].text)
             count += 1
             if count >= 3:
@@ -41,7 +42,7 @@ def medieAn(date):
     medie = PcData()
     count = 0
 
-    for i in range(1, 13):
+    for i in [6, 11]:
         luna = medieLuna(date + "/" + str(i))
         if luna:
             medie.add(luna)
@@ -50,8 +51,31 @@ def medieAn(date):
     medie.divide(count)
     return medie
 
-medie = medieAn("2019")
-print("cpre  count: ", medie.cores)
-print("rmax  count: ", medie.rmax)
-print("rpeak count: ", medie.rpeak)
-print("power count: ", medie.power)
+#medie = medieAn("2019")
+#print("cpre  count: ", medie.cores)
+#print("rmax  count: ", medie.rmax)
+#print("rpeak count: ", medie.rpeak)
+#print("power count: ", medie.power)
+
+medii = []
+ani = [i for i in range(1993, 2021)]
+
+for i in ani:
+    medii.append(medieAn(str(i)))
+    print(f"medie anul {i} done")
+
+index = 0
+
+print("rmax values:")
+year = ani[0]
+
+while index < len(medii) - 1:
+
+    print(f"anul: {ani[index]}-{ani[index+1]} progres: {medii[index+1].rmax - medii[index].rmax}")
+
+    index += 1
+
+#print("cpre  count: ", medie.cores)
+#print("rmax  count: ", medie.rmax)
+#print("rpeak count: ", medie.rpeak)
+#print("power count: ", medie.power)
