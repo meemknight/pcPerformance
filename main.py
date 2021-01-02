d4e0c96fd5be4e43b42b3d49064685d1b6bb58eb
vlod <vladalex420@gmail.com>
2021-01-02 15:48:51 +0200

    removed pygame

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests
 from pprint import pprint
 from bs4 import BeautifulSoup
 from pcData import *
-import pygame
 
 def medieLuna(date):
     r = requests.get('https://www.top500.org/lists/top500/' + date)
@@ -13,8 +12,16 @@ def medieLuna(date):
     soup = BeautifulSoup(r.text, 'lxml')
 
     table = soup.find('table', class_="table table-condensed table-striped")
-
     tableRow = table.find_all('tr') 
+
+    tableHeader = soup.find('thead')
+    tableHeaderRow = tableHeader.find('tr').find_all('th')
+    tableMeasurement = tableHeaderRow[3].text
+    
+    divisor = 1
+    if 'GFlop/s' in tableMeasurement:
+        divisor = 1000
+
 
     count = 0
     medie = PcData()
@@ -22,11 +29,14 @@ def medieLuna(date):
     for i in tableRow:
 
         tableData = i.find_all('td')
+        
+
 
         if len(tableData) > 2:
+
             medie.cores +=  int(tableData[2].text.replace(',',""))
-            medie.rmax +=   float(tableData[3].text.replace(',',""))
-            medie.rpeak +=  float(tableData[4].text.replace(',',""))
+            medie.rmax +=   float(tableData[3].text.replace(',',"")) / divisor
+            medie.rpeak +=  float(tableData[4].text.replace(',',"")) / divisor
             #medie.power +=  float(tableData[5].text.replace(',',""))
             #print(tableData[2].text)
             count += 1
@@ -66,16 +76,20 @@ for i in ani:
 
 index = 0
 
-print("rmax values:")
+print("rmax values(tflops/s):")
 year = ani[0]
+
+rmaxMedie = 0
 
 while index < len(medii) - 1:
 
     print(f"anul: {ani[index]}-{ani[index+1]} progres: {medii[index+1].rmax - medii[index].rmax}")
-
+    rmaxMedie = medii[index+1].rmax - medii[index].rmax
     index += 1
 
 #print("cpre  count: ", medie.cores)
 #print("rmax  count: ", medie.rmax)
 #print("rpeak count: ", medie.rpeak)
 #print("power count: ", medie.power)
+
+print(f"Medie = {rmaxMedie / index}")
